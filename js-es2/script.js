5ced72614b40a6285ede501d40ac332975696fc1
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-04-19 12:44:35 +0200

    Fine esercizio dadi, migliorata parte estetica con boot4

--- js-es2/script.js
+++ js-es2/script.js
@@ -1,19 +1,23 @@
 var lancio1p1= Math.floor(Math.random()*6)+1;
 var lancio2p1= Math.floor(Math.random()*6)+1;
 player1score = lancio1p1+lancio2p1;
-document.writeln(player1score);
+document.getElementById("player1l1").innerHTML=lancio1p1;
+document.getElementById("player1l2").innerHTML=lancio2p1;
+document.getElementById("player1").innerHTML=player1score;
 
 var lancio1p2= Math.floor(Math.random()*6)+1;
 var lancio2p2= Math.floor(Math.random()*6)+1;
 player2score = lancio1p2+lancio2p2;
-document.writeln(player2score);
+document.getElementById("player2l1").innerHTML=lancio1p2;
+document.getElementById("player2l2").innerHTML=lancio2p2;
+document.getElementById("player2").innerHTML=player2score;
 
 if (player1score>player2score) {
-  document.writeln("Complimenti Player1, hai vinto!");
+  document.getElementById("result").innerHTML="Complimenti Player1, hai vinto!";
 }
 if (player1score==player2score) {
-  document.writeln("Totale equilibrio, Pareggio!");
+  document.getElementById("result").innerHTML="Totale equilibrio, Pareggio!";
 }
 if(player2score>player1score){
-  document.writeln("Complimenti Player2, hai vinto!");
+  document.getElementById("result").innerHTML="Complimenti Player2, hai vinto!";
 }
